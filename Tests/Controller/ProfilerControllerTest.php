98db27cc774d919a968a0309341f02b2371a003f
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-04-13 16:11:12 +0200

    CS fixes - native_function_invocation & static_lambda

--- Tests/Controller/ProfilerControllerTest.php
+++ Tests/Controller/ProfilerControllerTest.php
@@ -188,7 +188,7 @@ class ProfilerControllerTest extends WebTestCase
         $profiler
             ->expects($this->exactly(2))
             ->method('loadProfile')
-            ->willReturnCallback(fn ($token) => 'found' == $token ? new Profile($token) : null)
+            ->willReturnCallback(static fn ($token) => 'found' == $token ? new Profile($token) : null)
         ;
 
         $controller = $this->createController($profiler, $twig, $withCsp);
@@ -529,7 +529,7 @@ class ProfilerControllerTest extends WebTestCase
             ->with($expectedTemplate);
 
         $this
-            ->createController($profiler, $twig, false, array_map(function (string $collectorName) use ($expectedPanel, $expectedTemplate): array {
+            ->createController($profiler, $twig, false, array_map(static function (string $collectorName) use ($expectedPanel, $expectedTemplate): array {
                 if ($collectorName === $expectedPanel) {
                     return [$expectedPanel, $expectedTemplate];
                 }
